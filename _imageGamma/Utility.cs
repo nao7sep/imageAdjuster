41da2aeb3e9728c2c26c300a4d7140f737bd624b
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-09-30 16:04:28 +0900

    Relative luminance

---
+++ _imageGamma/Utility.cs
@@ -0,0 +1,8 @@
+﻿namespace _imageGamma
+{
+    public static class Utility
+    {
+        // https://en.wikipedia.org/wiki/Relative_luminance
+        public static double GetLuminance (byte red, byte green, byte blue) => 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+    }
+}
