f4cee7033386947dfde0786b5ea2e49588777291
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-09-30 23:18:18 +0900

    GetContrastStretchingLimits

--- _imageGamma/Utility.cs
+++ _imageGamma/Utility.cs
@@ -33,5 +33,86 @@ namespace _imageGamma
 
             return xHistogramData;
         }
+
+        /// <summary>
+        /// Both MinValue and MaxValue are inclusive, meaning that they are the lower and upper bounds of the range that should be preserved.
+        /// </summary>
+        public static (int MinValue, int MaxValue) GetContrastStretchingLimits (int [] histogramData, int totalPixelCount, double lowerCutoffPercentage, double upperCutoffPercentage)
+        {
+            int xMinValue = 0;
+
+            if (lowerCutoffPercentage >= 0)
+            {
+                int xLowerCutoffPixelCount = (int) Math.Round (totalPixelCount * lowerCutoffPercentage / 100),
+                    xCurrentPixelCount = 0;
+
+                for (int temp = 0; temp <= 255; temp ++)
+                {
+                    if ((xCurrentPixelCount += histogramData [temp]) > xLowerCutoffPixelCount)
+                    {
+                        xMinValue = temp;
+                        break;
+                    }
+                }
+            }
+
+            int xMaxValue = 255;
+
+            if (upperCutoffPercentage >= 0)
+            {
+                int xUpperCutoffPixelCount = (int) Math.Round (totalPixelCount * upperCutoffPercentage / 100),
+                    xCurrentPixelCount = 0;
+
+                for (int temp = 255; temp >= 0; temp --)
+                {
+                    if ((xCurrentPixelCount += histogramData [temp]) > xUpperCutoffPixelCount)
+                    {
+                        xMaxValue = temp;
+                        break;
+                    }
+                }
+            }
+
+            if (xMinValue >= xMaxValue)
+            {
+                xMinValue = 0;
+                xMaxValue = 255;
+            }
+
+            return (xMinValue, xMaxValue);
+        }
+
+    #if DEBUG
+        public static void TestGetContrastStretchingLimits ()
+        {
+            static void _Test (int [] firstPart, int [] lastPart, double lowerCutoffPercentage, double upperCutoffPercentage)
+            {
+                int [] xHistogramData = new int [256];
+                Array.Copy (firstPart, 0, xHistogramData, 0, firstPart.Length);
+                Array.Copy (lastPart, 0, xHistogramData, 256 - lastPart.Length, lastPart.Length);
+
+                var xLimits = GetContrastStretchingLimits (xHistogramData, totalPixelCount: 100, lowerCutoffPercentage, upperCutoffPercentage);
+
+                Console.WriteLine ($"First: {string.Join (", ", firstPart)}, Last: {string.Join (", ", lastPart)}, Lower: {lowerCutoffPercentage}, Upper: {upperCutoffPercentage} => Min: {xLimits.MinValue}, Max: {xLimits.MaxValue}");
+            }
+
+            // The following results are from a GetContrastStretchingLimits without the xMinValue >= xMaxValue check.
+
+            _Test ([0, 1, 2], [2, 1, 0], -1, -1); // => Min: 0, Max: 255
+            _Test ([0, 1, 2], [2, 1, 0], 0, 0); // => Min: 1, Max: 254
+            _Test ([0, 1, 2], [2, 1, 0], 1, 1); // => Min: 2, Max: 253
+            _Test ([0, 1, 2], [2, 1, 0], 2, 2); // => Min: 2, Max: 253
+
+            _Test ([0, 1, 2], [2, 1, 0], 3, 3); // => Min: 253, Max: 2
+
+            _Test ([100], [], -1, -1); // => Min: 0, Max: 255
+            _Test ([100], [], 0, 0); // => Min: 0, Max: 0
+            _Test ([100], [], 1, 1); // => Min: 0, Max: 0
+
+            _Test ([], [100], -1, -1); // => Min: 0, Max: 255
+            _Test ([], [100], 0, 0); // => Min: 255, Max: 255
+            _Test ([], [100], 1, 1); // => Min: 255, Max: 255
+        }
+    #endif
     }
 }
